00a4ee634a9b2e26f0ca6f1082e298b86a11e011
unknown <dipak1920@gmail.com>
2023-10-17 18:37:04 +0545

    removed unnecessary div from button element

--- calculator.js
+++ calculator.js
@@ -11,4 +11,24 @@ document.addEventListener('DOMContentLoaded',()=>{
     function divide(a,b){
         return a / b
     }
+
+    let first_number;
+    let operator;  
+    let second_number; 
+
+    function operate(first_number, operator,second_number){
+        if (operator == "+"){
+            add(first_number,second_number)
+        }
+        else if (operator == "-"){
+            subtract(first_number,second_number)
+        }
+        else if (operator == "*"){
+            multiply(first_number,second_number)
+        }
+        else{
+            divide(first_number,second_number)
+        }
+    }
+    operate(8,"+",9)
 })
