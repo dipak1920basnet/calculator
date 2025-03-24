a6bc972e39fec5e4eed7cc5ada45acbe5838f3ac
dipak1920basnet <dipak1920basnet@gmail.com>
2025-03-24 11:04:03 +0545

    adds functionality of normal calculation like add, subtract, multiply, divide

--- calculator.js
+++ calculator.js
@@ -10,25 +10,54 @@ document.addEventListener('DOMContentLoaded',()=>{
     }
     function divide(a,b){
         return a / b
-    }
-
-    let first_number;
-    let operator;  
-    let second_number; 
+    } 
 
     function operate(first_number, operator,second_number){
         if (operator == "+"){
-            add(first_number,second_number)
+            return(add(first_number,second_number))
         }
         else if (operator == "-"){
-            subtract(first_number,second_number)
+            return(subtract(first_number,second_number))
         }
         else if (operator == "*"){
-            multiply(first_number,second_number)
+            return(multiply(first_number,second_number))
         }
         else{
-            divide(first_number,second_number)
+            return(divide(first_number,second_number))
         }
     }
-    operate(8,"+",9)
+    
+    document.querySelectorAll(".number").forEach(element => {
+        element.addEventListener("click", function()
+    {
+        a = document.querySelector(".events")
+        a.innerHTML  = a.innerHTML + `${this.value}`
+    })
+    });
+
+    document.querySelectorAll(".key-operator").forEach(element => {
+        element.addEventListener("click", function()
+    {
+        a = document.querySelector(".events")
+        a.innerHTML  = `${a.innerHTML}${this.value}`
+    })
+    })
+
+    document.querySelector(".key--equal").addEventListener("click",function(){
+        a = document.querySelector(".events")
+        let first
+        first = Array.from(a.innerText)
+        console.log(first)
+        let m;
+        m = first.indexOf("+")
+        console.log(first.slice(0,m))
+        let result = operate(parseInt(first.slice(0,m).join("")), first[m], parseInt(first.slice(m+1).join("")))
+        console.log(result)
+        a.innerHTML  = " "+ result
+
+    })
+
+    document.querySelector(".clear").addEventListener("click", function(){
+        document.querySelector(".events").innerHTML = ""
+    })
 })
